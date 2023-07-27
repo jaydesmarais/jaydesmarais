26c37987a214fe1f829b8810cbc4a0782f29028e
Jay Desmarais <jydsmrs6@gmail.com>
2023-07-27 14:52:18 -0600

    Adding warning and link to temp portfolio

--- jaydesmarais-react-app/src/App.js
+++ jaydesmarais-react-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './caution.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Personal Portfolio Under Construction
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jaydesmarais.carrd.co"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Boring Temporary Website
         </a>
       </header>
     </div>
